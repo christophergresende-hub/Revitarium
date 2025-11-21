9ed73b554df45655d6716efe5e15ad8f670ac87a
christophergresende-hub <christopher.g.resende@gmail.com>
2025-11-21 13:21:57 -0300

    Update app.js

--- Revitarium/app/app.js
+++ Revitarium/app/app.js
@@ -1,17 +1,3 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const user = document.getElementById("user").value.trim();
-  const pass = document.getElementById("pass").value.trim();
-
-  if (user === "" || pass === "") {
-    alert("Preencha todos os campos!");
-    return;
-  }
-
-  if (user === "admin" && pass === "123") {
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Usuário ou senha incorretos!");
-  }
+document.getElementById("logout").addEventListener("click", () => {
+  window.location.href = "index.html";
 });
